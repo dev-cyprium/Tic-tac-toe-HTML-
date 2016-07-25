3c9f59a4aa40a55c8d7b43f8b5158db1d74942fa
destroyergm <stefan_vg@hotmail.com>
2016-07-25 19:51:37 +0200

    Add x and o html data

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,8 +5,29 @@ var Game = function() {
 	}
 };
 
+var Mark = function() {
+	/*
+		State can be EMPTY, X or O
+	*/
+	this.state = "EMPTY";
+}
+
+/*
+	X html data
+*/
+Mark.$ohtml = $("<div>", {class: "mark-o"})
+	.append($("<div>", {class: "inner-o"}));
+
+/*
+	O html data
+*/
+Mark.$xhtml = $("<div>", {class: "mark-x"})
+	.append($("<div>", {class: "left-part-x"}))
+	.append($("<div>", {class: "right-part-x"}));
 
 $(document).ready(function() {
 	var game = new Game();
-	
+
+	$("#cell-0-0").append(Mark.$ohtml);
+	$("#cell-0-1").append(Mark.$xhtml);
 });
