d350d75cec89476f5b0bcec2745a2f18e1be3651
destroyergm <stefan_vg@hotmail.com>
2016-07-25 20:32:13 +0200

    Finish game prototype

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,65 @@
 var Game = function() {
+	/*
+		Initialize the game board
+	*/
 	this.board = new Array(3);
 	for(var i=0; i < 3; i++) {
 		this.board[i] = new Array(3);
 	}
+
+	for(var i=0; i < 3; i++) {
+		for(var j=0; j < 3; j++) {
+			this.board[i][j] = new Mark();
+		}
+	}
+
+	// Active turn tracking, first player is X
+	this.activeTurn = "X";
+
+	/*
+		Add Click action listeners to all the divs
+	*/
+
+	var gamereferance = this;
+	$(".cell").each(function() {
+		$(this).click(function() {
+			var row = parseInt(this.id.charAt(5));
+			var col = parseInt(this.id.charAt(7));
+			if(gamereferance.board[row][col].state != "EMPTY") {
+				alert("wrong move!");
+			} else {
+				gamereferance.board[row][col].state = gamereferance.activeTurn;
+				gamereferance.nextTurn();
+				gamereferance.render();
+			}
+		});
+	});
+
 };
+
+Game.prototype.render = function() {
+	for(var i=0; i < 3; i++) {
+		for(var j=0; j < 3; j++) {
+			switch(this.board[i][j].state) {
+				case "X":
+					$("#cell-"+i+"-"+j).html(Mark.$xhtml.clone());
+					break;
+				case "O":
+					$("#cell-"+i+"-"+j).html(Mark.$ohtml.clone());
+					break;
+			}
+		}
+	}
+}
+
+Game.prototype.nextTurn = function() {
+	this.activeTurn = (this.activeTurn == "X") ? "O" : "X";
+	$("#current-player").html(this.activeTurn).css("color",Game.getSymbolColor.call(this, this.activeTurn));
+}
+
+Game.getSymbolColor = function() {
+	return (this.activeTurn == "X") ? Mark.xColor : Mark.oColor;
+}
 
 var Mark = function() {
 	/*
@@ -13,21 +69,22 @@ var Mark = function() {
 }
 
 /*
-	X html data
+	X jQuery object, clone it to use it
 */
 Mark.$ohtml = $("<div>", {class: "mark-o"})
 	.append($("<div>", {class: "inner-o"}));
 
 /*
-	O html data
+	O jQuery object, clone it to use it
 */
 Mark.$xhtml = $("<div>", {class: "mark-x"})
 	.append($("<div>", {class: "left-part-x"}))
 	.append($("<div>", {class: "right-part-x"}));
 
+Mark.xColor = "crimson";
+Mark.oColor = "#33adff";
+
 $(document).ready(function() {
 	var game = new Game();
-
-	$("#cell-0-0").append(Mark.$ohtml);
-	$("#cell-0-1").append(Mark.$xhtml);
+	$("#current-player").html("X").css("color",Mark.xColor);
 });
