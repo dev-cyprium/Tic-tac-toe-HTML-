134d71b5224237cf0a739128e043ff7bf67d8909
destroyergm <stefan_vg@hotmail.com>
2016-07-25 19:07:40 +0200

    Add game.js file and jQuery to the project

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var Game = function() {
+	this.board = new Array(3);
+	for(var i=0; i < 3; i++) {
+		this.board[i] = new Array(3);
+	}
+};
+
+
+$(document).ready(function() {
+	var game = new Game();
+	
+});
